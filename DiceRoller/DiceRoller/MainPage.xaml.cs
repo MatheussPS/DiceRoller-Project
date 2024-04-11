8d8057d3259b3ab9a41e1ee76dc78562e9abd515
Etec Prof. Horácio Augusto da Silveira <matheuspinter08@gmail.com>
2024-04-11 12:26:18 -0300

    feat: exercício finalizado

---
+++ DiceRoller/DiceRoller/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace DiceRoller
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void sort_NumberBtn_Clicked(object sender, EventArgs e)
+        {
+            Random random = new Random();
+
+            var picker = (Picker)qtd_LadosPicker;
+
+            try
+            {
+                int selectedIndex = picker.SelectedIndex;
+
+                string pickerValue = Convert.ToString(picker.ItemsSource[selectedIndex]);
+
+                int sortedNumber = random.Next(1, Convert.ToInt32(pickerValue));
+
+                sorted_NumberLabel.Text = Convert.ToString(sortedNumber);
+            }
+            catch
+            {
+                sorted_NumberLabel.Text = "Selecione uma opção";
+            }
+
+            
+
+
+        }
+    }
+
+}
